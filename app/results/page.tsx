32f7bfbe479d200a47be5987961205088de73201
Pablo Zallio <pablozalliodev@gmail.com>
2026-06-30 11:03:41 +0200

    [STYLE] - Setting Up Basic Fonts and Colors

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,9 +1,9 @@
 
 
 export default function Results() {
-  return (
-      <section className="">
-            <p className="">Pagina de resultados</p>
-      </section>
-  )
+      return (
+            <section className=" bg-background text-whiteText p-10 ">
+                 
+            </section>
+      )
 }
