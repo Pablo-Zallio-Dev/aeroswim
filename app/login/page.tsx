2ff88b6904352c35cf691f411df6e9aa7e09e163
Pablo Zallio <pablozalliodev@gmail.com>
2026-07-08 09:48:10 +0200

    [FEAT - LOGIN] - Add links to login

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Login() {
+
+
+
+      return (
+            <section className=" pt-40 w-full m-auto px-6 xl:px-6 max-w-350  ">
+                
+            </section>
+      )
+}
